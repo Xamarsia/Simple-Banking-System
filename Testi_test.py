fa3588b5542d11703dc53a1c568852aa65b15676
Xamarsia <xamarsia@gmail.com>
2020-06-28 16:59:23 +0300

    initialization: add welcome messenger for a user; feat: add capability to calculate how long you need to repay a credit

---
+++ Testi_test.py
@@ -0,0 +1,38 @@
+import math
+
+credit_principal = input('''What do you want to calculate? 
+type "n" - for count of months, 
+type "a" - for annuity monthly payment,
+type "p" - for credit principal:''')
+if credit_principal == 'n':
+    money = int(input('Enter credit principal:'))  # P
+    monthly_payment = int(input('Enter monthly payment:'))  # A
+    credit_interest = int(input('Enter credit interest:'))  # i  процент
+    i = (0.01 * credit_interest / 12)
+    n = math.log((monthly_payment / (monthly_payment - i * money)), 1 + i)
+    n = round(n, 1)
+    if (str(n))[-1] >= '5':
+        n = math.ceil(n)
+    else:
+        n = math.floor(n)
+    year = n // 12
+    month = n - year * 12
+    if year > 1:
+        year = str(year) + " " + "years"
+    elif year == 1:
+        year = "1 year"
+    else:
+        year = ''
+
+    if month > 1:
+        month = str(month) + " " + "months"
+    elif month == 1:
+        month = "1 month"
+    else:
+        month = ''
+    if month != 0 and year != 0:
+        final = "You need {} and {} to repay this credit!".format(year, month)
+    else:
+        final = "You need {}{} to repay this credit!".format(year, month)
+    print(final)
+
