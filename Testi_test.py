fa645ec0b333fc23d1aa9a3aeada7c702b2a02db
Xamarsia <xamarsia@gmail.com>
2020-06-28 20:39:20 +0300

    feat: add capability to calculate your annuity payment

--- Testi_test.py
+++ Testi_test.py
@@ -7,14 +7,11 @@ type "p" - for credit principal:''')
 if credit_principal == 'n':
     money = int(input('Enter credit principal:'))  # P
     monthly_payment = int(input('Enter monthly payment:'))  # A
-    credit_interest = int(input('Enter credit interest:'))  # i  процент
+    credit_interest = float(input('Enter credit interest:'))  # i  процент
     i = (0.01 * credit_interest / 12)
     n = math.log((monthly_payment / (monthly_payment - i * money)), 1 + i)
     n = round(n, 1)
-    if (str(n))[-1] >= '5':
-        n = math.ceil(n)
-    else:
-        n = math.floor(n)
+    n = math.ceil(n)
     year = n // 12
     month = n - year * 12
     if year > 1:
@@ -35,6 +32,14 @@ if credit_principal == 'n':
     else:
         final = "You need {}{} to repay this credit!".format(year, month)
     print(final)
+elif credit_principal == 'a':
+    money = float(input('Enter credit principal:'))  # P СЗ
+    periods = int(input('Enter count of periods:'))  # n СК
+    credit_interest = float(input('Enter credit interest:'))  # i  процент П
+    i = (0.01 * credit_interest / 12)
+    val = ((1 + i) ** periods)
+    A = math.ceil(money * ((i * val)/(val - 1)))
+    print("Your annuity payment = " + str(A) + "!")
 elif credit_principal == "p":
     monthly_payment = float(input('Enter monthly payment:'))  # A
     periods = int(input("Enter count of periods:")) # n
